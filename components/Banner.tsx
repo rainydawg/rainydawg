d13a4bc7b9f6ee0570975bb9e805ec1068f5a274
unknown <asuwrdtm@uw.edu>
2021-10-18 15:01:22 -0700

    removed DJ application link from Banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,13 +6,13 @@ function Banner() {
     <div className={styles.banner}>
       <h2>Announcement</h2>
       <p>
-        <a
+        {/* <a
           href="https://docs.google.com/forms/d/e/1FAIpQLSdzT8jrkmFWr2CZYiElJ6_Bu5diDQWTC-LILVqg7Vp1mtFqDQ/viewform?vc=0&c=0&w=1&flr=0"
           target="_blank"
           rel="noreferrer"
         >
           DJ Application OPEN!
-        </a>
+        </a> */}
         <a
          href="https://docs.google.com/forms/d/e/1FAIpQLSfgKuFuvccRhJP8JaHGXK9FGeNYQo4X_fbWxDMCCjQ4Amdq9A/viewform?usp=sf_link
          "
