84782933c6a742e66a4d38eb47394b370180ec47
RainyDawgCooper <asuwrdtm@uw.edu>
2022-03-11 17:26:55 -0800

    banner update

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,19 +6,19 @@ function Banner() {
     <div className={styles.banner}>
       <h2>Announcement</h2>
       <p>
-        {/* <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSdzT8jrkmFWr2CZYiElJ6_Bu5diDQWTC-LILVqg7Vp1mtFqDQ/viewform?vc=0&c=0&w=1&flr=0"
+        { <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSfNcyNrU595Qyy31GhBXW-2wKGAbCPjl7x2RHjeNFxAvqXt-g/viewform"
           target="_blank"
           rel="noreferrer"
         >
-          DJ Application OPEN!
-        </a> */}
+          DJ Application OPEN!      
+        </a> }
         <a
          href="https://docs.google.com/forms/d/e/1FAIpQLSfgKuFuvccRhJP8JaHGXK9FGeNYQo4X_fbWxDMCCjQ4Amdq9A/viewform?usp=sf_link
          "
          target="_blank"
          rel="noreferrer">
-         Blogger Application Open!
+          Blogger Application Open! 
         </a>
       </p>
     </div>
