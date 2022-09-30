85495eb03294e6953f6efdaacec992ec83cff62d
rainydawg <asuwrdtm@uw.edu>
2022-09-30 12:57:20 -0700

    DJ Application

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,16 +4,8 @@ function Banner() {
   console.log(styles);
   return (
     <div className={styles.banner}>
-      <h2>Announcement</h2>
-      <p>
-        {/* { <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSfNcyNrU595Qyy31GhBXW-2wKGAbCPjl7x2RHjeNFxAvqXt-g/viewform"
-          target="_blank"
-          rel="noreferrer"
-        > */}
-          DJ Applications are CLOSED!      
-        </a>
-      </p>
+      <h2>Anncoument</h2>
+      <p>DJ Applications are CLOSED!</p>
     </div>
   );
 }
