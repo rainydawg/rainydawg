28394043ae4c6856012e255d38805f53a8645961
MiaPham2520 <mia2520@uw.edu>
2023-11-09 10:01:51 -0800

    Update application banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,7 +5,8 @@ function Banner() {
   return (
     <div className={styles.banner}>
       <h2>Announcement</h2>
-      <p>DJ Appliciations are Closed</p>
+      {/* <p>DJ Appliciations are Closed</p> */}
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLSelv72v3Tz0HRiD2DrBPzkJqm-CTboDCm-p08QFy3VmcCnxSw/viewform" >DJ Applicalications are Open</a>
     </div>
   );
 }
